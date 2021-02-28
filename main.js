5cd51b6fc99b14371898a68f842aad2a37484b6b
JonasMatray <56248551+JonasMatray@users.noreply.github.com>
2021-02-28 19:59:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -17,13 +17,17 @@ function req_hp(id) { // fonction appelé au click d'un bouton avec l'id de l'el
         console.log("Échec de chargement " + url_hp);
     };
     req_hp.onload = function () {
+
         if (req_hp.status === 200) {
+
             var data_hp = JSON.parse(req_hp.responseText); // les données sont enregistrés dans la variable data_hp
             console.log(data_hp);
             //utiliser les "data" pour faire nos requetes
+
         } else {
             console.log("Erreur " + req_hp); // log d'erreur en cas d'echec de récupération
         }
+
         var value = document.getElementById(id); //récuperer l'element bouton
         value.innerHTML = data_hp[id].Horsepower; // afficher la valeur a la place du texte du bouton
     }
@@ -31,25 +35,29 @@ function req_hp(id) { // fonction appelé au click d'un bouton avec l'id de l'el
 }
 
 req_cars.onload = function () {
+
     if (req_cars.status === 200) {
+
         data = JSON.parse(req_cars.responseText); // enregistrement des donnees dans var Data
         console.log(data);
         //utiliser les "data" pour faire nos requetes
+
     } else {
         console.log("Erreur " + req_cars.status); // console log en cas d'erreur
     }
+
     for (let i = 0; i < data.length; i += 1) { //parcours de toute la liste des voitures
 
-            var newTR = document.createElement("tr"); //création de l'element tr (nouvelle ligne)
-            newTR.id = "line_" + i.toString();
-            newTR.innerHTML = "<td>" + data[i].Name + "</td> <td>" + data[i].Miles_per_Gallon + "</td> <td>" + data[i].Cylinders + "</td> <td>" + data[i].Displacement + "</td> <td>" + data[i].Weight_in_lbs + "</td> <td>" + data[i].Acceleration + "</td> <td>" + data[i].Year + "</td> <td>" + data[i].Origin + "</td> <button id='" + i + "' onclick='req_hp(" + i + ")'>Get HP</button>";//Ajout du contenu de cette ligne
-            document.getElementById("table_cars").appendChild(newTR); //Ajout de la ligne au tableau
+        var newTR = document.createElement("tr"); //création de l'element tr (nouvelle ligne)
+        newTR.id = "line_" + i.toString();
+        newTR.innerHTML = "<td>" + data[i].Name + "</td> <td>" + data[i].Miles_per_Gallon + "</td> <td>" + data[i].Cylinders + "</td> <td>" + data[i].Displacement + "</td> <td>" + data[i].Weight_in_lbs + "</td> <td>" + data[i].Acceleration + "</td> <td>" + data[i].Year + "</td> <td>" + data[i].Origin + "</td> <button id='" + i + "' onclick='req_hp(" + i + ")'>Get HP</button>";//Ajout du contenu de cette ligne
+        document.getElementById("table_cars").appendChild(newTR); //Ajout de la ligne au tableau
 
     }
 };
 req_cars.send(); //envoie de la requète voiture
 
-window.onload = function() { //chargement de la fenetre avec la fonction en référence
+window.onload = function () { //chargement de la fenetre avec la fonction en référence
 
     const searchName = document.getElementById('search_input'); //récupération de l'input nom
     const searchAccel = document.getElementById('accel_input'); //récupération de l'input accel
@@ -58,42 +66,38 @@ window.onload = function() { //chargement de la fenetre avec la fonction en réf
     searchName.addEventListener('input', function (event) { //
 
         searchGeneral(searchName, searchAccel); // appel de la fonction qui filtre les elements du tableau
-
     });
 
     searchAccel.addEventListener('input', function (event) { //
 
         searchGeneral(searchName, searchAccel); // appel de la fonction qui filtre les elements du tableau
-
     });
 
-
-    searchBtn.addEventListener('click', function(event) { // action a l'evenement clique sur le bouton
+    searchBtn.addEventListener('click', function (event) { // action a l'evenement clique sur le bouton
 
         //la fonction étais dans les consigne mais grace a notre amélioration, elle devient obselète.
         //Nous l'avons laissez uniquement parce qu'elle est mentionnée dans les consignes.
 
         searchGeneral(searchName, searchAccel); // appel de la fonction qui filtre les elements du tableau
-
     });
 }
 
 
-
-
 function searchGeneral(searchName, searchAccel) {
+
     let linesCars = document.querySelectorAll("tr"); //renvoie tous les éléments qui correspondent au selecteur tr
+
     for (let line of linesCars) {
 
         if (!(line.id === '')) {
-            let lineName = line.firstChild.textContent.toLowerCase();
-            let lineAccel = parseFloat(line.children[5].textContent);
-            if (!lineName.match(searchName.value.toLowerCase()) || (!(lineAccel < searchAccel.value) && !(searchAccel.value.toString() === ''))) {
-                line.style.visibility = "collapse";
+            let lineName = line.firstChild.textContent.toLowerCase(); // récupération du champ nom de la ligne
+            let lineAccel = parseFloat(line.children[5].textContent); // récupération du champ acceleration de lal ligne
+
+            if (!lineName.match(searchName.value.toLowerCase()) || (!(lineAccel < searchAccel.value) && !(searchAccel.value.toString() === ''))) { // vérification des conditions pour les deux input
+                line.style.visibility = "collapse"; //enlevement des lignes qui ne corresponde pas à la recherche
             } else {
-                line.style.visibility = "visible";
+                line.style.visibility = "visible"; //affichage des lignes qui corresponde à la recherche
             }
         }
     }
-
 }
